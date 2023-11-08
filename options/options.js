02e4f7c7708e55115f17a6d3a46a54166ec76cd3
Crowdbotics <team@crowdbotics.com>
2023-11-08 23:28:55 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "drinksforme-44430",
+  url: "https://drinksforme-44430.botics.co",
+  api: "https://drinksforme-44430.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
